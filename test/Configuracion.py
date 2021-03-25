5589ae4496cdb3f389783d153116b8add216847a
ocorzo <47366913+ocorzo@users.noreply.github.com>
2021-03-24 20:23:57 -0600

    Update Configuracion.py

--- test/Configuracion.py
+++ test/Configuracion.py
@@ -6,3 +6,5 @@ Compresion = False # Define si los datos que se almacenarán localmente serán c
 EnvioAlaNube = True # Define si los datos serán enviados a Internet a traves del protocolo MQTT
 TxRxPins = [33, 19] # Los pines donde está conectado el sensor [33, 19] para cancay a1, 
 LogEnTerminal = False # Habilita el envío de log a la consola repl
+
+# Esta es solo una línea para probar el OTA
