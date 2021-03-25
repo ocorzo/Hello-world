e744d0bd70fc55742486b2fe90aa016d93fa501c
ocorzo <47366913+ocorzo@users.noreply.github.com>
2021-03-24 19:56:11 -0600

    Add files via upload

---
+++ Configuracion.py
@@ -0,0 +1,8 @@
+# Definición de parámetros de configuración
+
+PeriodoDePoleo = 30000 # Milisegundos entre cada poleo de mediciones
+AlmacenamientoLocal = False # Define si los datos se alamacenarán localmente o no
+Compresion = False # Define si los datos que se almacenarán localmente serán comprimidos
+EnvioAlaNube = True # Define si los datos serán enviados a Internet a traves del protocolo MQTT
+TxRxPins = [33, 19] # Los pines donde está conectado el sensor [33, 19] para cancay a1, 
+LogEnTerminal = False # Habilita el envío de log a la consola repl
